2ab286553f67374bb2a00a9ba0d2cabf24c76d31
luizlucchetta <142125268+luizlucchetta@users.noreply.github.com>
2024-09-29 22:59:37 -0300

    Add files via upload

---
+++ Trabalho/idioma.php
@@ -0,0 +1,15 @@
+<?php
+
+$lingua = $_GET['lingua'];
+
+if (isset($lingua)) {
+    //Configurando o cookie com o valor passado
+    setcookie('idioma', $lingua, time() + (60*60*24*3)); //Cria o cookie que expira em 3 dias
+} else {
+    //Apagando o cookie
+    setcookie('idioma', '', 1);
+}
+
+//Redireciona para a pagina index.php
+header('Location: index.php');
+
---
+++ Trabalho/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBDESIGNER</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="titulo">
+        <h1><center>BEM VINDO AO SITE WEBDESIGNER EM FOCO</center></h1>
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="conteudo">
+            <h2><center>Escolha um idioma para entrar</center></h2>
+
+        </div>
+
+        <div>
+       
+        <a href="idioma.php?lingua=portugues">
+            <img src=imagens/brasil width=50px height=50px>
+        </a> &nbsp;&nbsp;
+
+        <a href="idioma.php?lingua=ingles">
+            <img src="imagens/reino-unido.jpeg" alt="" style="width:50px;height:50px;">
+        </a> &nbsp;&nbsp;
+
+        <a href="idioma.php?lingua=espanhol">
+            <img src="imagens/espanha.jpeg" alt="" style="width:50px;height:50px;">
+        </a> &nbsp;&nbsp;
+</div>
+
+
+</body>
+</html>
